ba12eb8d7266a5301ba6a1e58aaeb0f58e5e4e2e
S1ngularty <asherxd10245@gmail.com>
2026-01-11 16:41:10 +0800

    get all and one document resource service

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -2,12 +2,26 @@ const User = require("../models/userModel");
 const { uploadImage } = require("../utils/cloundinaryUtil");
 
 exports.update = async (request) => {
-  const  userId  = "6961e1e6cc915cb55f6c8b5c";
-  if (request.file) request.body.avatar = await uploadImage([request.file],'users');
+  const userId = req.user.userId;
+  if (request.file)
+    request.body.avatar = await uploadImage([request.file], "users");
   const user = await User.findByIdAndUpdate(userId, request.body, {
     new: true,
     runValidators: true,
   });
   if (!user) throw new Error("failed to update the user");
+  return user;
+};
+
+exports.getAll = async (request) => {
+  const { userId } = request.user;
+  const users = await User.find({ userId: { $ne: userId } });
+  return users;
+};
+
+exports.getById = async (request) => {
+  const { userId } = request.params;
+  const user = await User.findById(userId);
+  if (!user) throw new Error("user is not found in the collection");
   return user;
 };
