a2303647ac58cd73258aaed9da83c1e456c7ab16
S1ngularty <asherxd10245@gmail.com>
2026-07-31 21:58:38 +0800

    user module types and model

---
+++ backend/modules/users/user.types.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+interface IAvatar<T> {
+  public_id: T;
+  url: T;
+}
+
+interface ILoyaltyHistory {
+  event: "earn" | "redeem";
+  points: number;
+  date: Date;
+}
+
+interface IEligibilityDiscountUsage {
+  discountUsed: number;
+  purchasedUsed: number;
+  weekStart: Date;
+  weekEnd: Date;
+}
+
+export interface IUser {
+  firebaseUid?: string;
+  name: string;
+  email: string;
+  password?: string | null;
+
+  sex: "male" | "female" | null;
+  age?: number;
+  birthDate: Date;
+
+  address?: string;
+  street?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  zipCode?: string;
+
+  contactNumber?: string;
+
+  avatar?: IAvatar<string>;
+
+  role: "user" | "admin" | "super_admin" | "checker" | "merchandiser";
+
+  loyaltyPoints: number;
+
+  loyaltyHistory: ILoyaltyHistory[];
+  eligibilityDiscountUsage: IEligibilityDiscountUsage;
+
+  savedItems: mongoose.Schema.Types.ObjectId[];
+
+  status: "active" | "inactive";
+
+  lastLogin: Date;
+
+  resetPasswordToken?: string;
+  resetPasswordExpire?: Date;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IUserMethods {
+  getToken(): string | null;
+  updateLastLogin(): Promise<void>;
+}
