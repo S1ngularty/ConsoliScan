04b75082e3b32962c4afe5196104168eefc653d6
S1ngularty <asherxd10245@gmail.com>
2026-08-03 20:22:59 +0800

    auth module login controller

--- backend/modules/auth/auth.controller.ts
+++ backend/modules/auth/auth.controller.ts
@@ -1,6 +1,10 @@
 import type { Request, Response, NextFunction } from "express";
 import * as AuthService from "./auth.service.js";
-import type { RegisterPayload, ReturnAuthPayload } from "./auth.types.js";
+import type {
+  LoginPayload,
+  RegisterPayload,
+  ReturnAuthPayload,
+} from "./auth.types.js";
 import type { ApiResponse } from "../../types/api.js";
 
 export const register = async (
@@ -11,7 +15,24 @@ export const register = async (
   try {
     const result = await AuthService.register(req.body);
 
-    res.status(201).json({
+    res.status(200).json({
+      message: "OK",
+      success: true,
+      result: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (
+  req: Request<{}, {}, LoginPayload>,
+  res: Response<ApiResponse<ReturnAuthPayload>>,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const result = await AuthService.login(req.body);
+    res.status(200).json({
       message: "OK",
       success: true,
       result: result,
