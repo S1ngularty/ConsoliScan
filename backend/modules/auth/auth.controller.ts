e6ad2d152fe9e4d8dc44bf07e62c573172357ce1
S1ngularty <asherxd10245@gmail.com>
2026-08-03 20:36:30 +0800

    auth module logout and verify token controller

--- backend/modules/auth/auth.controller.ts
+++ backend/modules/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import type { Request, Response, NextFunction } from "express";
 import * as AuthService from "./auth.service.js";
 import type {
+  JWTtokenProperties,
   LoginPayload,
   RegisterPayload,
   ReturnAuthPayload,
+  UserAuthProperties,
 } from "./auth.types.js";
 import type { ApiResponse } from "../../types/api.js";
 
@@ -36,6 +38,40 @@ export const login = async (
       message: "OK",
       success: true,
       result: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const verifyToken = async (
+  req: Request<{}, {}, JWTtokenProperties>,
+  res: Response<ApiResponse<ReturnAuthPayload>>,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const result = await AuthService.verifyToken(req.body);
+    res.status(200).json({
+      message: "OK",
+      success: true,
+      result: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (
+  req: Request<{}, {}, UserAuthProperties>,
+  res: Response<ApiResponse<null>>,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const result = await AuthService.logout(req.body);
+    res.status(200).json({
+      message: "OK",
+      success: true,
+      result: null,
     });
   } catch (error) {
     next(error);
