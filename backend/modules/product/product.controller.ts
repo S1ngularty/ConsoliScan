c47c075498ccfbd78f1a93f3347b5834227cdfbf
S1ngularty <asherxd10245@gmail.com>
2026-08-10 21:35:42 +0800

    product module added create controller layer and response wrapper utility

---
+++ backend/modules/product/product.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, NextFunction, Response } from "express";
+import { wrapResponse } from "../../utils/response.util.js";
+import * as ProductService from "./product.service.js";
+import type { EditableProductFields, IProduct } from "./product.types.js";
+import type { ApiResponse } from "../../types/api.js";
+
+export const create = async (
+  req: Request<{}, {}, Omit<EditableProductFields, "images">>,
+  res: Response<ApiResponse<IProduct>>,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { body, files } = req;
+    if (!body) throw new Error("empty product body");
+    const imageFiles = Array.isArray(files) ? files : undefined;
+
+    const result = await ProductService.create(body, imageFiles);
+
+    wrapResponse<IProduct>("ok", 200, res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
