8e712f9338615770c2b3e8c4cc730cc23ac313eb
S1ngularty <asherxd10245@gmail.com>
2026-08-14 15:23:02 +0800

    Catalog service layer

--- backend/modules/catalog/catalog.service.ts
+++ backend/modules/catalog/catalog.service.ts
@@ -1,2 +1,14 @@
 import type { ICatalog } from "./catalog.types.js";
+import * as CategoryRepository from "./catalog.repository.js";
 
+export const getVersion = async (): Promise<Number> => {
+  const version = await CategoryRepository.getCatalogVersion();
+
+  return version;
+};
+
+export const bumpVersion = async (): Promise<Number> => {
+  const version = await CategoryRepository.bumpCatalog();
+
+  return version;
+};
