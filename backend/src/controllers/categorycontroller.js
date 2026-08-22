73aeec04ba70f92812e8584eb6a789cb024fc21e
S1ngularty <leviashpenaverde@gmail.com>
2026-08-22 22:18:37 +0800

    files that is not necessary for v2 of consoliscan

--- backend/src/controllers/categorycontroller.js
+++
@@ -1,7 +0,0 @@
-const categoryService = require("../services/categoryService");
-const controllerWrapper  = require("../utils/controllerWrapper");
-
-exports.categoryList = controllerWrapper(categoryService.list);
-exports.addCategory = controllerWrapper(categoryService.create);
-exports.categoryUpdate = controllerWrapper(categoryService.update);
-exports.categoryDelete = controllerWrapper(categoryService.delete);
