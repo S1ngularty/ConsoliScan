4c1f08fb320c1f8b15da730b4a93c56ace72ab63
S1ngularty <asherxd10245@gmail.com>
2026-08-23 21:54:33 +0800

    index route

--- backend/src/modules/queue/queue.dto.ts
+++
@@ -1,7 +0,0 @@
-import type { IQueue } from "./queue.types.js";
-
-export interface IQueueCreate extends IQueue {}
-export interface IQueueCreateResponse extends Pick<IQueue, "checkoutCode"> {}
-
-export interface IGetQueueOrder extends Pick<IQueue, "checkoutCode"> {}
-export interface QueueOrderResponse extends IQueue {}
